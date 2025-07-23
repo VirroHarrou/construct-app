c5a1962a143cf2c0f758f3aba39050960d4a3145
virro <ya.vaniafot@yandex.ru>
2025-07-23 18:01:23 +0300

    added android secure flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+
 import 'package:construct/services/token/token_storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await _preventScreenshot();
 
   runApp(
     ProviderScope(
@@ -15,4 +20,14 @@ void main() async {
       child: MyApp(),
     ),
   );
+}
+
+Future<void> _preventScreenshot() async {
+  if (Platform.isAndroid) {
+    try {
+      await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+    } catch (e) {
+      debugPrint("Ошибка блокировки скриншотов: $e");
+    }
+  }
 }
